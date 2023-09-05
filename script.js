6414cc302c42dd5bdcc49dbdadc4010e2942b321
Olga Bogdanova <bon@incom-sr.ru>
2023-09-05 08:54:17 +0300

    итог

--- script.js
+++ script.js
@@ -14,20 +14,12 @@ const addContainer = document.getElementById("add-container");
 let currentActiveCard = 0;
 
 const cardsEl = [];
-const cardsData = [
-  {
-    question: "What must a variable begin with?",
-    answer: "A letter, $ or _",
-  },
-  {
-    question: "What is a variable?",
-    answer: "Container for a piece of data",
-  },
-  {
-    question: "Example of Case Sensitive Variable",
-    answer: "thisIsAVariable",
-  },
-];
+const cardsData = getCardsData();
+
+function getCardsData() {
+  const cards = JSON.parse(localStorage.getItem("cards"));
+  return cards === null ? [] : cards;
+}
 
 function createCards() {
   cardsData.forEach((card, index) => createCard(card, index));
@@ -84,4 +76,37 @@ prevBtn.addEventListener("click", () => {
   updateCurrentText();
 });
 
+showBtn.addEventListener("click", () => {
+  addContainer.classList.add("show");
+});
 
+hideBtn.addEventListener("click", () => {
+  addContainer.classList.remove("show");
+});
+addCardBtn.addEventListener("click", () => {
+  const question = questionEl.value;
+  const answer = answerEl.value;
+  if (question.trim() && answer.trim()) {
+    const newCard = {
+      question,
+      answer,
+    };
+    createCard(newCard);
+    questionEl.value = "";
+    answerEl.value = "";
+    addContainer.classList.remove("show");
+    cardsData.push(newCard);
+    setCardsData(cardsData);
+  }
+});
+//в localstorage
+function setCardsData(data) {
+  localStorage.setItem("cards", JSON.stringify(data));
+  window.location.reload();
+}
+
+clearBtn.addEventListener("click", () => {
+  localStorage.clear();
+  cardsContainer.innerHTML = "";
+  window.location.reload()
+});
